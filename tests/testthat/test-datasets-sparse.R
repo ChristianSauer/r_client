f6458cf2d8bb8f9e18f2a19b35e8f6ca5be14fbf
Paweł Biernat <pawel.biernat@comma-soft.de>
2019-01-16 17:19:01 +0100

    More tests

--- tests/testthat/test-datasets-sparse.R
+++ tests/testthat/test-datasets-sparse.R
@@ -12,6 +12,24 @@ gen_data <- function(){
     return(list(cells=cell_metadata, genes=gene_metadata, matrix=matrix))
 }
 
+gen_args <- function(replacements){
+    default_conn <- new(
+        "FGConnection", base_url=BASE_URL, bearer_token=BEARER_FROM_ENV)
+    input = gen_data()
+    args = list(connection = default_conn,
+                matrix = input$matrix,
+                cell_metadata = input$cells,
+                gene_metadata = input$genes,
+                title = "R client test",
+                description = "description",
+                short_description = "short_description",
+                organism_id = 9606,
+                gene_nomenclature = "GeneSymbol",
+                tmpdir="./tmpdata")
+    args[names(replacements)] = replacements
+    args
+}
+
 test_that(
     "create-sparse: can create a dataset", {
         default_conn <- new(
@@ -28,4 +46,46 @@ test_that(
                                     gene_nomenclature = "GeneSymbol",
                                     tmpdir="./tmpdata")
         expect_is(result, "FGResponse")
+    })
+
+test_that(
+    "create-sparse: wrong matrix type", {
+        args <- gen_args(list(matrix = "abc"))
+        expect_error(do.call(create_dataset_df, args), "Unsupported matrix format, expected a \"dgTMatrix\".")
+    })
+
+test_that(
+    "create-sparse: wrong cell_metadata type", {
+        args <- gen_args(list(cell_metadata = "abc"))
+        expect_error(do.call(create_dataset_df, args), "cell_metadata must be a data frame.")
+    })
+
+test_that(
+    "create-sparse: wrong gene_metadata type", {
+        args <- gen_args(list(gene_metadata = "abc"))
+        expect_error(do.call(create_dataset_df, args), "gene_metadata must be a data frame.")
+    })
+
+test_that(
+    "create-sparse: cell_metadata has no cellId", {
+        args <- gen_args(list(cell_metadata = data.frame()))
+        expect_error(do.call(create_dataset_df, args), "cell_metadata must have a cellId column.")
+    })
+
+test_that(
+    "create-sparse: gene_metadata has no geneId", {
+        args <- gen_args(list(gene_metadata = data.frame()))
+        expect_error(do.call(create_dataset_df, args), "gene_metadata must have a geneId column.")
+    })
+
+test_that(
+    "create-sparse: no common cell names", {
+        args <- gen_args(list(cell_metadata = data.frame(cellId=c(1,2,3))))
+        expect_error(do.call(create_dataset_df, args), "No common cell names found in matrix and cell_metadata.")
+    })
+
+test_that(
+    "create-sparse: no common gene names", {
+        args <- gen_args(list(gene_metadata = data.frame(geneId=c(1,2,3))))
+        expect_error(do.call(create_dataset_df, args), "No common gene names found in matrix and gene_metadata.")
     })
